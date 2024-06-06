476fad59aaa0f55aa09e5e4a7e11f37fd61ee3e4
Fab <rostellofabian@gmail.com>
2024-06-06 14:25:45 +0200

    fixed command line bug

--- index.js
+++ index.js
@@ -17,38 +17,38 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, 'GuildVoiceStates']
 });
 
-// // List of all commands
-// const commands = [];
-// client.commands = new Collection();
+// List of all commands
+const commands = [];
+client.commands = new Collection();
 
-// const commandsPath = path.join(__dirname, "commands"); // E:\yt\discord bot\js\intro\commands
-// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-// for (const file of commandFiles) {
-//     const filePath = path.join(commandsPath, file);
-//     const command = require(filePath);
+const commandsPath = path.join(__dirname, "commands"); // E:\yt\discord bot\js\intro\commands
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-//     client.commands.set(command.data.name, command);
-//     commands.push(command.data.toJSON());
-// }
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
+}
 
-// // Construct and prepare an instance of the REST module
-// const rest = new REST().setToken(process.env.TOKEN);
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.TOKEN);
 
-// // and deploy your commands!
-// (async () => {
-// 	try {
-// 		// The put method is used to fully refresh all commands in the guild with the current set
-// 		const data = await rest.put(
-//             Routes.applicationCommands(process.env.CLIENT_ID),
-//             { body: commands },
-//         );
+// and deploy your commands!
+(async () => {
+	try {
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands },
+        );
 
-// 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-// 	} catch (error) {
-// 		// And of course, make sure you catch and log any errors!
-// 		console.error(error);
-// 	}
-// })();
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
 
 // Add the player on the client
 client.player = new Player(client, {
